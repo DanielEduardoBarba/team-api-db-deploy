44f029e653c52531b81c9ed694f82fdcce9d9ae7
Edilma Riano <edilmaria@hotmail.com>
2023-01-27 15:01:31 -0500

    create the index routes. Test them and ready to go

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,22 @@
-const functions = require("firebase-functions");
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+import functions from "firebase-functions";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+
+// setup routes
+app.get('/ReadContact', (req, res) => {
+  res.send('This is a new contact');
+});
+
+app.get('/test2', (req, res) => {
+  res.send('This is another get paage');
+});
+
+app.post("/AddContact", (req, res)=> {
+    res.send ('post a contact')
+})
+
+export const api = functions.https.onRequest(app);
