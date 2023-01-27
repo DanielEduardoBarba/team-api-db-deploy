bca119493875d24adeb005484c3ac03b700f10d5
Daniel Barba <frdb103@gmail.com>
2023-01-27 14:29:00 -0500

    dbconnect ready

---
+++ functions/src/dbConnect.js
@@ -0,0 +1,10 @@
+import { initializeApp, cert} from "firebase-admin/app";
+import{getFirestore} from "firebase-admin/firestore"
+import { service_account } from "./secrets.js";
+
+initializeApp({
+    credential: cert(service_account)
+})
+
+export const dataBase = getFirestore()
+
